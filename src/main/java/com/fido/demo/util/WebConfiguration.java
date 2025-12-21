ba139032e882e653ac991168a08961fc2d7fdb6e
Ravikanth <ravikanthreddy89@gmail.com>
2025-12-21 14:41:49 -0800

    Move origins to application.properties

--- src/main/java/com/fido/demo/util/WebConfiguration.java
+++ src/main/java/com/fido/demo/util/WebConfiguration.java
@@ -1,6 +1,7 @@
 package com.fido.demo.util;
 
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -8,21 +9,15 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfiguration implements WebMvcConfigurer {
 
+    @Value("${demo.cors.allowed-origins:}")
+    private String[] allowedOrigins;
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                //.allowedOrigins("http://example.com")
-                //.allowedOrigins("*")
-                .allowedOrigins("https://www.sowmya.com",
-                        "http://demo.com",
-                        "https://ravikanth-fidotesting.netlify.app",
-                        "https://fidotesting-vercel.vercel.app",
-                        "https://fidotesting.vercel.app",
-                        "https://web2.fidotesting.com",
-                        "https://fidotesting.edgecompute.app")
+                .allowedOrigins(allowedOrigins)
                 .allowCredentials(true)
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("*");
     }
 }
-
